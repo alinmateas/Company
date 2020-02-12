9cd72c941dd1407a3831ffc5e50be24044d4f298
alinmateas <alin_10messi@yahoo.com>
2020-02-12 18:08:30 +0200

    First commit

---
+++ src/test/java/com/example/companygithub/CompanyGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companygithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
